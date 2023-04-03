e214c27e68b3d90aba3f9fa866470def13cf8ccc
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-04-03 16:48:16 +0900

    2023-04-03 퀴즈

---
+++ stswork3/SpringmvcQuiz/src/main/java/spring/mvc/dto/SawonDto.java
@@ -0,0 +1,42 @@
+package spring.mvc.dto;
+
+public class SawonDto {
+
+	private String name;
+	private String depart;
+	private int price;
+	private int age;
+	private String blood;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getBlood() {
+		return blood;
+	}
+	public void setBlood(String blood) {
+		this.blood = blood;
+	}
+}
