0689fe7062108b9997a372a50903c0a05abfd76d
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2023-04-14 12:25:01 +0900

    2023-04-14

---
+++ stswork4/JpaFileUploadProject/src/test/java/board/jpa/upload/JpaFileUploadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package board.jpa.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFileUploadProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
